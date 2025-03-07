22a64693e73f3a6a4769bbc387d4227a677c3fe4
gizopp <giovanazoppelaro@gmail.com>
2025-03-06 22:55:21 -0300

    feat: theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  darkBlue: "#1a237e",
+  lightBlue: "#12C2E9",
+  neon: "#A5FF32",
+  white: "#fff",
+  black: "#000",
+};
+
+export const fontSize = {
+  h1: 28,
+};
+
+export const theme = {
+  colors,
+  fontSize,
+};
+
+export default theme;
