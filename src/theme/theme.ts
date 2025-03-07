56597e68495cd6f8a420d8537c428d0fa2216923
gizopp <giovanazoppelaro@gmail.com>
2025-03-07 09:26:28 -0300

    feat: theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,9 @@
-export const colors = {
+import {
+  PTSansCaption_400Regular,
+  PTSansCaption_700Bold,
+} from "@expo-google-fonts/pt-sans-caption";
+
+const colors = {
   darkBlue: "#1a237e",
   lightBlue: "#12C2E9",
   neon: "#A5FF32",
@@ -6,13 +11,31 @@ export const colors = {
   black: "#000",
 };
 
-export const fontSize = {
+const fontSize = {
   h1: 28,
+  h4: 18,
 };
 
-export const theme = {
+const borderRadius = {
+  default: 12,
+  big: 50,
+};
+
+const fontFamily = {
+  regular: "PTSansCaption-Regular",
+  bold: "PTSansCaption-Bold",
+};
+
+const fonts = {
+  "PTSansCaption-Regular": PTSansCaption_400Regular,
+  "PTSansCaption-Bold": PTSansCaption_700Bold,
+};
+
+const theme = {
   colors,
   fontSize,
+  fontFamily,
+  borderRadius,
 };
 
 export default theme;
